75f7e5b81379830043ef2f4f07810d28013b4936
Marc Vilagrosa <mvila2004@gmail.com>
2025-12-23 12:44:13 +0100

    Práctica 3 - Implementada

--- js/main.js
+++ js/main.js
@@ -15,3 +15,37 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
+// VARIABLES DEL MODAL
+const modal = document.getElementById("pdfModal");
+const iframe = document.getElementById("pdfFrame");
+const downloadLink = document.getElementById("downloadLink");
+const closeBtn = document.getElementsByClassName("close-btn")[0];
+
+// FUNCIÓN PARA ABRIR EL PDF
+function openPDF(path) {
+    modal.style.display = "block";
+    iframe.src = path; // Carga el PDF en el iframe
+    downloadLink.href = path; // Configura el botón de descarga
+    
+    // Evita que se mueva la página de fondo
+    document.body.style.overflow = "hidden"; 
+}
+
+// CERRAR AL PULSAR LA X
+closeBtn.onclick = function() {
+    closeModal();
+}
+
+// CERRAR AL PULSAR FUERA DEL CONTENIDO (EN LO OSCURO)
+window.onclick = function(event) {
+    if (event.target == modal) {
+        closeModal();
+    }
+}
+
+function closeModal() {
+    modal.style.display = "none";
+    iframe.src = ""; // Limpia el iframe para que no siga cargando oculto
+    document.body.style.overflow = "auto"; // Reactiva el scroll
+}
